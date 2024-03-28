5013153165832a748b978f79d4221fba239d4214
Buezrello <igindin@gmail.com>
2024-03-28 10:36:04 +0200

    redis event listener

--- src/main/java/org/compie/listener/DatabaseEventListener.java
+++ src/main/java/org/compie/listener/DatabaseEventListener.java
@@ -1,25 +1,25 @@
-//package org.compie.listener;
-//
-//import org.compie.model.balldontlie.Data;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.event.EventListener;
-//import org.springframework.data.redis.core.RedisTemplate;
-//import org.springframework.stereotype.Component;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//@Component
-//public class DatabaseEventListener {
-//
-//    @Autowired
-//    private RedisTemplate<String, Object> redisTemplate;
-//
-//    @EventListener
-//    @Transactional(readOnly = true)
-//    public void handleDatabaseChange(Object event) {
-//        if (event instanceof Data) {
-//            Data data = (Data) event;
-//            redisTemplate.convertAndSend("databaseChanges",
-//                    "Database change detected: " + data.getId());
-//        }
-//    }
-//}
+package org.compie.listener;
+
+import org.compie.model.balldontlie.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class DatabaseEventListener {
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @EventListener
+    @Transactional(readOnly = true)
+    public void handleDatabaseChange(Object event) {
+        if (event instanceof Data) {
+            Data data = (Data) event;
+            redisTemplate.convertAndSend("databaseChanges",
+                    "Database change detected: " + data.getId());
+        }
+    }
+}
